491d02698c73a523846dc9771e7aa9c46650df1b
BunthongRorn <bunthongran579@gmail.com>
2025-06-13 14:30:41 +0700

    Initailize project

---
+++ website/src/app/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <h1 className="text-4xl font-bold bg-blue-500 w-100 h-100 border border-red-400 border-solid rounded-2xl">This is homepage</h1>
+  );
+}
