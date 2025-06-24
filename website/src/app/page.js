e73ad16b5ab824b0edab0ac204bcdc6ca8f30df8
BunthongRorn <bunthongran579@gmail.com>
2025-06-24 13:18:30 +0700

    Add top navbar and menu in the header

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import TopNav from '../components/Header/TopNav/TopNav';
 import Menu from "@/components/Header/Menu/Menu";
 
+
 export default function Home() {
   return (
     <div className="overflow-hidden">
