6950cd0a55acab1d605f48882e9aaeb33066dcda
BunthongRorn <bunthongran579@gmail.com>
2025-08-03 12:20:17 +0700

    Feat: connect frontend with backend

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Add this 'images' object
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/upload/**",
+      },
+    ],
+  },
+};
+
 
 export default nextConfig;
